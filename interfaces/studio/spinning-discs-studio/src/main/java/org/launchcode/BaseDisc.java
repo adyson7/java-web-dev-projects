422a5115ee19001e2055256b1e476048504df27b
AD <142710797+adyson7@users.noreply.github.com>
2024-01-08 17:52:41 -0600

    Disc Studio start

---
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/BaseDisc.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+public abstract class BaseDisc {
+
+    private String name;
+
+    private int capacity;
+
+    private String contents;
+
+    private String discType;
+}
+
+
